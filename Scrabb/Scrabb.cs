2381c9226ceea99192ac7d2b103388c77253480a
Noah Lundquist <nalundquist@gmail.com>
2022-09-22 11:17:51 -0700

    add UI functionality
    Co-authored-by: Ted Gold <ted.jonathan.gold@gmail.com>
    co-authored-by: Helen Lehrer <hlehrer@outlook.com>

--- Scrabb/Scrabb.cs
+++ Scrabb/Scrabb.cs
@@ -2,13 +2,17 @@ using System;
 using System.Collections.Generic;
 using Scrabb.Score;
 
-namespace Scrabb
+namespace ScrabUI
 {
-	public class Program
-	{
-		public static void Main()
-		{
-
-		}
-	}
+  public class Program
+  {
+    public static void Main() 
+    {
+      Console.WriteLine("Type your scrabble word below to determine your score:");
+      string word = Console.ReadLine();
+			SScore letterScore = new SScore();
+      Console.WriteLine("Your word score is: " + letterScore.SScoreTab(word));
+      Main();
+    }
+  }
 }
