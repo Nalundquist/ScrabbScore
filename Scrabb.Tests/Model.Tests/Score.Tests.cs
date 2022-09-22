feb41fb0bd1773c6867547ef93646c15413a1561
Noah Lundquist <nalundquist@gmail.com>
2022-09-22 09:31:40 -0700

    add boilerplate
    Co-authored-by: Ted Gold <ted.jonathan.gold@gmail.com>
    Co-authored-by: Helen Lehrer <hlehrer@outlook.com>

---
+++ Scrabb.Tests/Model.Tests/Score.Tests.cs
@@ -0,0 +1,12 @@
+using System;
+using Scrabb.Score;
+
+namespace Scrabb.Score.Test
+{
+	[TestClass]
+	public class SScoreTests
+	{
+		[TestMethod]
+		public void 
+	}
+}
---
+++ Scrabb/Scrabb.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using Scrabb.Score;
+
+namespace Scrabb
+{
+	public class Program
+	{
+		public static void Main
+		{
+			
+		}
+	}
+}
